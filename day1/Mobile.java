1896cbffa2dcb845681e6a5290b0189b15ea8305
AshnaLinkesh <125642367+AshnaLinkesh@users.noreply.github.com>
2023-02-16 23:53:55 -0800

    Add files via upload
    Day1

---
+++ day1/Mobile.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Mobile {
+	public void makeCall() {
+ System.out.println("hi my name is Ashna");
+	}
+public void sendMsg() {
+	System.out.println("welcome");
+}
+	public static void main(String[] args) {
+		Mobile ob1=new Mobile();
+ob1.makeCall();
+
+ob1.sendMsg();
+	}
+
+}
---
+++ day1/Twowheeler.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Twowheeler {
+	
+	public void twowheel() {
+	int noofwheels=2;
+			short noofMirrors =2;
+			long Chassisnumber=678;
+			boolean isPunctured;
+			String bikename= "Lamaha";
+			Double runningKM=4.900;
+			
+	}
+	public static void main(String[] args) {
+		Twowheeler b= new Twhowheeler();
+		b.twowheel();
+		System.out.println(noofMirrors);
+		System.out.println(Chassisnumber);
+		System.out.println(isPunctured);
+		System.out.println(bikename);
+		System.out.println(runningKM);
+		
+	}
+
+}
